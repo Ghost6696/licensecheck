246e264cbfaf4a2321f74bd51097305550ec8940
Your Name <you@example.com>
2026-03-22 16:54:11 +0200

    feat: Add DELETE method to allowed CORS headers.

--- worker.js
+++ worker.js
@@ -10,7 +10,7 @@ export default {
     // 1. Handle CORS Preflight (OPTIONS)
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
       "Access-Control-Max-Age": "86400",
     };
